49b73a965f5ac7d8849fb77c0ffeab0e695f8ba2
Lukeria <lkarina30082000@gmail.com>
2025-06-01 15:02:46 +0000

    fix

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s! v2";
+	private static final String template = "Hello, %s! v3";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/greeting")
