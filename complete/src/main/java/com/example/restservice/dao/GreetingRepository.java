53819c7d38814688f4abbffc2fcf711218b922e2
lukeria <lkarina30082000@gmail.com>
2025-06-03 14:06:50 +0200

    Attempt to deploy to GKE without db

--- complete/src/main/java/com/example/restservice/dao/GreetingRepository.java
+++ complete/src/main/java/com/example/restservice/dao/GreetingRepository.java
@@ -2,13 +2,20 @@ package com.example.restservice.dao;
 
 import com.example.restservice.Greeting;
 import org.springframework.stereotype.Repository;
+
+import java.util.concurrent.atomic.AtomicLong;
 //import org.springframework.data.jpa.repository.JpaRepository;
 
 //public interface GreetingRepository extends JpaRepository<Greeting, Long> {
 //}
 @Repository
 public class GreetingRepository {
+
+    private final AtomicLong counter = new AtomicLong();
+
     public Greeting save(Greeting greeting) {
-        return greeting;
+        Greeting newGreeting = new Greeting(greeting.getContent());
+        newGreeting.setId(counter.incrementAndGet());
+        return newGreeting;
     }
 }
