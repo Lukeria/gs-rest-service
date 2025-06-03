9f731a444ae14361eebb3cbbb29bff0d17791350
lukeria <lkarina30082000@gmail.com>
2025-06-03 13:04:30 +0200

    remove db

--- complete/src/main/java/com/example/restservice/dao/GreetingRepository.java
+++ complete/src/main/java/com/example/restservice/dao/GreetingRepository.java
@@ -1,7 +1,14 @@
 package com.example.restservice.dao;
 
 import com.example.restservice.Greeting;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GreetingRepository extends JpaRepository<Greeting, Long> {
+//public interface GreetingRepository extends JpaRepository<Greeting, Long> {
+//}
+@Repository
+public class GreetingRepository {
+    public Greeting save(Greeting greeting) {
+        return greeting;
+    }
 }
